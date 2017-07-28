083668b0583f885f1a2efb407cb34fa85efea187
winxos <winxos@hotmail.com>
2017-07-28 12:47:58 +0800

    modify udp_client, for future multi-agent simulation. added system explain to README.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # AISTLab IOT DEMO
+> A simplest framework of IOT
+
+> Demo is simulation of MOBIKE system
+
+> winxos 2017-07-20
+
+## System Elements
+1. UDP SERVER (OR MQTT EQ)
+2. ARDUINO DEVICE(ESP8266)
+3. HTTP SERVER(USING FLASK IN DEMO)
+4. APP(USING WEB BROWSER)
+
+## Sequence
+1. User scan the QR code in DEVICE. The QR code encode the http request contain server host and device id or others.
+2. App deal the Authorization, and pass the id to http server.
+3. Http server pass through the command to the UDP server(OR MQTT server).
+4. Each DEVICE keep alive with UDP server.
+5. UDP server route the command to the DEVICE through ID.
+6. After the Device received the command, unlock, send 'unlocked' message to the UDP server.
+7. UDP server pass through the message to the HTTP server.
+8. HTTP server pass through 'unlocked' message to App.
--- udp_client.py
+++ udp_client.py
@@ -1,15 +1,58 @@
 import socket
+import threading
+import time
 
-HOST, PORT = "localhost", 9999
+server_host, server_port = "localhost", 9999
 data = "id=1233456;cmd=login"
 
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+is_exit = False
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(bytes(data + "\n", "utf-8"), (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
 
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+class getcmd(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+
+    def run(self):
+        while not is_exit:
+            try:
+                cmd = input()
+                self.sock.sendto(bytes(cmd, 'utf8'), (server_host, server_port))
+            except Exception as e:
+                print("[shell err] %s" % e)
+
+
+class listener(threading.Thread):
+    def __init__(self, sock):
+        threading.Thread.__init__(self)
+        self.sock = sock
+
+    def run(self):
+        while not is_exit:
+            try:
+                data, addr = self.sock.recvfrom(1024)
+                print("[debug] %s:%s" % (addr, data))
+            except Exception as e:
+                print("[listen err] %s" % e)
+
+
+if __name__ == '__main__':
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', 10001))
+    sock.sendto(bytes(data, 'utf8'), (server_host, server_port))
+    t = getcmd(sock)
+    t.setDaemon(True)  # important
+    t.start()
+    l = listener(sock)
+    l.setDaemon(True)
+    l.start()
+    print("listening...")
+    try:
+        while t.isAlive() and l.isAlive():
+            pass
+    except KeyboardInterrupt:
+        print("[sys err] user stop.")
+    is_exit = True
+    print("server exit.")
+    t.join()
+    l.join()
